7ff8598c20400676dcb09d34a3c68a2fb19bef59
checker-bh <shamlooh122@gmail.com>
2024-09-08 12:52:45 +0300

    ex2

--- exercises.py
+++ exercises.py
@@ -3,4 +3,9 @@ def calculate_area_triangle(base, height):
     return (base * height) / 2
 
 print('Exercise 1:', calculate_area_triangle(10, 5))
+# Exercise 2:
+def simple_interest(principal, rate, time):
+    return (principal * rate * time) / 100
+
+print('Exercise 2:', simple_interest(1000, 5, 2))
 
